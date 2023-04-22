cafad514ed0a94ebf407fbfe226f1cf8b223e12d
ThanhTam1234a <116643903+ThanhTam1234a@users.noreply.github.com>
2023-04-22 15:02:26 +0700

    bt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { ReactDOM } from 'react';
+// function Example() {
+//   // Khai báo 1 biến trạng thái mới đặt tên là "count"
+//   const [count, setCount] = useState(0);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//   return (
+//     <div>
+//       <p>Bạn đã click {count} lần</p>
+//       <button onClick={() => setCount(count + 1)}>
+//         Click vào tôi
+//       </button>
+//     </div>
+//   );
+// }
+// function FavoriteColor(){
+//   const [color, setColor] = useState("red");
+//   return(
+//    <div>
+//       <h1> My favorite color is {color}!</h1>
+//       <button
+//         type="button"
+//         onClick={() =>setColor("blue")}
+//       >Blue
+//       </button>
+//       <button
+//         type="button"
+//         onClick={() =>setColor("red")}
+//       >red
+//       </button>
+//       <button
+//         type="button"
+//         onClick={() =>setColor("pink")}
+//       >pink
+//       </button>
+//       <button
+//         type="button"
+//         onClick={() =>setColor("green")}
+//       >green
+//       </button>
+//   </div>
+//   );
+// }'
+
+function Car() {
+  const [car,setCar]=  useState({
+    brand :"Ford",
+    model :"Mustang",
+    year :"1544",
+    color :"red"
+  }); 
+  return(
+    <div>
+      <h1>My{car.brand}</h1>
+      <p>It is a{car.color}{car.model}from {car.year}.</p>
     </div>
-  );
+  )
 }
-
-export default App;
+export default Car;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import FavoriteColor from './App';
+import Car from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <FavoriteColor></FavoriteColor> */}
+    <Car></Car>
   </React.StrictMode>
 );
 
